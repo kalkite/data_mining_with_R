c8bf9e60f43341cfa44e88a318d71293641a7147
kalkite <rajeshkalakoti@gmail.com>
2023-08-28 22:17:41 +0300

    R code

--- R/utils.R
+++ R/utils.R
@@ -73,8 +73,113 @@ stirling_second <- function(n, k) {
 
 
 
+library(pdist)
+library(donut)
+library(RANN)
+#' Hopkins Statistics
+#'
+#' \deqn{H = \frac{\sum_{i=1}^{r} \beta_i}{\sum_{i=1}^{r}(\alpha_i+\beta_i) }}
+#'
+#' @return Data (matrix or data.frame) to check clusterability.
+#' @export
+#'
+#'
+#' @examples
+#' set.seed(1)
+#' hopkins(iris[, 1:4], m=15) # .9952293
+#'
+#' hop <- rep(NA, 100)
+#' for(i in 1:100){
+#'   hop[i] <- hopkins(iris[,1:4], m=8)
+#' }
+#' mean(hop)
+hopkins_stat <- function (X, m=ceiling(nrow(X)/10), d=ncol(X), k=1, U=NULL, method="simple") {
 
+  if (!(is.matrix(X)) & !(is.data.frame(X)))
+    stop("X must be data.frame or matrix")
 
+  if (m >= nrow(X))
+    stop("m must be no larger than num of samples")
+
+  if(missing(U)) {
+    # U is a matrix of column-wise uniform values sampled from the space of X
+    colmin <- apply(X, 2, min)
+    colmax <- apply(X, 2, max)
+    U <- matrix(0, ncol = ncol(X), nrow = m)
+    for (i in 1:ncol(X)) {
+      U[, i] <- runif(m, min = colmin[i], max = colmax[i])
+    }
+  } else {
+    # The user has provided the uniform values in U.
+  }
+
+  # Random sample of m rows in X (without replacement)
+  j <- sample(1:nrow(X), m)
+  W <- X[j, , drop=FALSE]   # Need 'drop' in case X is single-column
+
+  if(method=="simple") {
+    # distance between each row of W and each row of X
+    dwx <- as.matrix(pdist(W,X))
+    # Caution: W[i,] is the same point as X[j[i],] and the distance between them is 0,
+    # but we do not want to consider that when calculating the minimum distance
+    # between W[i,] and X, so change the distance from 0 to Inf
+    for(i in 1:m) dwx[i,j[i]] <- Inf
+    # distance from each row of W to the NEAREST row of X
+    dwx <- apply(dwx, 1, min)
+
+    # distance between each row of U and each row of X
+    dux <- as.matrix(pdist(U,X)) # rows of dux refer to U, cols refer to X
+    # distance from each row of U to the NEAREST row of X
+    dux <- apply(dux, 1, min)
+
+    hop <- sum(dux^d) / sum( dux^d + dwx^d )
+  }
+  if(method=="torus") {
+    rng <- t(apply(X,2,range))
+
+    # Note: Since W is a sample from X, the 1st nearest point in X will
+    # always be the same point with distance 0, so add 1 to k.
+    nearw <- donut::nnt(X, W, k=k+1, torus=1:ncol(W), ranges=rng )
+    dwx <- nearw$nn.dists[,k+1]
+
+    # For U, find the 1st nearest point in X, k=1.
+    nearu <- donut::nnt(X, U, k=k, torus=1:ncol(W), ranges=rng )
+    dux <- nearu$nn.dists[,k]
+
+    hop <- sum(dux^d) / sum( dux^d + dwx^d )
+  }
+
+  if(method=="boundedsphere" | method=="boundedcube"){
+    # distance between each row of W and each row of X
+    dwx <- as.matrix(pdist(W,X))
+    for(i in 1:m) dwx[i,j[i]] <- Inf
+    # distance from each row of W to the NEAREST row of X
+    dwx <- apply(dwx, 1, min)
+
+    # distance between each row of U and each row of X
+    dux <- as.matrix(pdist(U,X)) # rows of dux refer to U, cols refer to X
+    dux <- apply(dux, 1, min)
+
+    if(method=="boundedsphere") const <- 1
+    if(method=="boundedcube") const <- 2
+    ukd <- (const*dux)^d
+    wkd <- (const*dwx)^d
+    N <- nrow(X)
+
+    # I *think* this is the formula of Rotondi, page 560-561.
+    # However, what happens if ukd=1? Then division by zero.
+    # Also, this is supposed to have Beta(kM,kM) distribution, so it should
+    # be between [0,1], but my example has values outside [0,1].
+    # Should X be scaled to unit hypercube/hypersphere???
+    hop <- (N-k+1) * sum( ukd/(1-ukd) ) /
+      sum( (N-k+1)*ukd/(1-ukd) + (N-k)*(wkd/(1-wkd)) )
+  }
+
+  # You would think this would be faster, but it is not for our test cases:
+  # stat = 1 / (1 + sum(dwx^d) / sum( dux^d ) )
+
+  return( hop )
+}
 
 
 
